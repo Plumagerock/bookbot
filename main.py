cd4e9d83d66af57805d06559e26d7c023c4a52cf
Plumagerock <plumagerock@gmail.com>
2024-10-15 15:17:59 -0400

    added notes for functions, added link to frankenstein text, added version

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# Bookbot project from Boot.dev
+#
+# Created by:   Ian Featherstone
+# Version:      1
+#
+
 def main():
+    # frankenstein file located here: https://raw.githubusercontent.com/asweigart/codebreaker/master/frankenstein.txt
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
     char_count = get_chars_dict(book_text)
-    print(char_count)
     sorted_char_count = get_chars_dict_to_sorted(char_count)
-
     print_report(book_path, word_count, sorted_char_count)
     
 # returns input book text
@@ -18,7 +23,7 @@ def get_word_count(book):
     word_count = book.split()
     return len(word_count)
 
-# returns characters from book as dict
+# returns characters from book as dict (converted to all lowercase)
 def get_chars_dict(book):
     chars = {}
     lower_book = book.lower()
@@ -31,15 +36,18 @@ def get_chars_dict(book):
                 chars[char] = 1
     return chars
 
+# returns list of sorted characters in format {char: num}
 def get_chars_dict_to_sorted(chars_dict):
     chars_sorted = []
     for char in chars_dict:
         chars_sorted.append({"char": char, "num": chars_dict[char]})
-    chars_sorted.sort(reverse=True, key=sort_on)
+    chars_sorted.sort(reverse=True, key=sort_key)
     return chars_sorted
 
+# prints report of book, word count, character freq
 def print_report(book, word, chars):
     print(f"--- Begin report of {book} ---")
+    print()
     print(f"{word} words found in document.")
     print()
     for char in chars:
@@ -48,7 +56,8 @@ def print_report(book, word, chars):
     print()
     print("--- End report ---")
 
-def sort_on(d):
+# return sort key by num
+def sort_key(d):
     return d["num"]
 
 main()
