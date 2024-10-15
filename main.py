5f1f83b83b4c7ae34c71ae6a6c372c618ef2f96c
Plumagerock <plumagerock@gmail.com>
2024-10-15 15:07:13 -0400

    modified get_ functions, added dict_to_sorted, report functions

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = get_word_count(text)
-    char_count = get_char_count(text)
-    print (text)
-    print (f"{word_count} words")
-    print(f"characters are: {char_count}")
-    
+    book_text = get_book_text(book_path)
+    word_count = get_word_count(book_text)
+    char_count = get_chars_dict(book_text)
+    print(char_count)
+    sorted_char_count = get_chars_dict_to_sorted(char_count)
 
+    print_report(book_path, word_count, sorted_char_count)
+    
+# returns input book text
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_word_count(path):
-    book = path.split()
-    return len(book)
+# returns length of book in number of words
+def get_word_count(book):
+    word_count = book.split()
+    return len(word_count)
 
-def get_char_count(book):
+# returns characters from book as dict
+def get_chars_dict(book):
     chars = {}
     lower_book = book.lower()
     book_words = lower_book.split()
@@ -26,7 +29,26 @@ def get_char_count(book):
                 chars[char] += 1
             else:
                 chars[char] = 1
-    
     return chars
+
+def get_chars_dict_to_sorted(chars_dict):
+    chars_sorted = []
+    for char in chars_dict:
+        chars_sorted.append({"char": char, "num": chars_dict[char]})
+    chars_sorted.sort(reverse=True, key=sort_on)
+    return chars_sorted
+
+def print_report(book, word, chars):
+    print(f"--- Begin report of {book} ---")
+    print(f"{word} words found in document.")
+    print()
+    for char in chars:
+        if char["char"].isalpha():
+            print(f"The {char['char']} character was found {char['num']} times.")
+    print()
+    print("--- End report ---")
+
+def sort_on(d):
+    return d["num"]
 
 main()
